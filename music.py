d3104babf4d31ce0ec9e097715c657f2d8fe5b1f
corbettlarsen <corbettlarsen@yahoo.com>
2016-06-03 07:27:13 -0600

    created buttonPressed variable so sound only plays once

--- music.py
+++ music.py
@@ -1,36 +1,20 @@
 import random, time, pygame, sys, serial_connect
-from pygame.locals import *
+
 
 def main():
-    
-    DISPLAYSURF = pygame.display.set_mode((50, 50))
+    pygame.mixer.pre_init(44100, -16, 12, 512)
     pygame.init()
-    pygame.mixer.music.load('sound/alarm.mp3')
+    sound1 = pygame.mixer.Sound('sound/hal_9000.wav')
     connect = serial_connect.ArduinoConnect()
-    counter = 0
-    canPlay = True
+    buttonPressed = 0
     while True:
         read = connect.readline()
-##        for event in pygame.event.get():
-##            if event.type == KEYDOWN:
-##                pygame.mixer.music.play(-1, 0.0)
-##            elif event.type == KEYUP:
-##                pygame.mixer.music.stop()
-##        if read:
-##            canPlay = True
-##        else:
-##            if canPlay == True:
-##                counter = counter + 1
-##            if counter > 100: ##how long?
-##                canPlay = False
-##            if canPlay == False:
-##                counter = 0
         if read:
-            pygame.mixer.music.play(0, 0.0)
-            
-        else:
-	    pygame.mixer.music.stop()
-	print read
-        pygame.display.update()
+	    if (buttonPressed == 0):
+		sound1.play()
+	    buttonPressed = 1
+	else:
+	    buttonPressed = 0
+        print read
 if __name__ == '__main__':
     main()
