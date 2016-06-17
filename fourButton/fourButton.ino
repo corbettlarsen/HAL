5b4209f0df756ee915e1f80cf8b42989fe1efb5f
corbettlarsen <corbettlarsen@yahoo.com>
2016-06-17 09:37:49 -0600

    added a port for the big red button

--- fourButton/fourButton.ino
+++ fourButton/fourButton.ino
@@ -7,6 +7,7 @@ void setup() {
   // initialize the LED pin as an output:
   pinMode(9, OUTPUT);      
   // initialize the pushbutton pin as an input:
+  pinMode(7,INPUT_PULLUP);
   pinMode(10, INPUT);
   pinMode(11, INPUT);
   pinMode(12, INPUT);
@@ -16,18 +17,21 @@ void setup() {
 
 void loop(){ 
   
-  unsigned char buttonState = 0;  
+  unsigned char buttonState = 0;
+  if(digitalRead(7)){
+    buttonState ^= 0b10000;
+  }  
   if(digitalRead(10)){
-    buttonState ^= 0b1000;
+    buttonState ^= 0b01000;
   }
    if(digitalRead(11)){
-    buttonState ^= 0b0100;
+    buttonState ^= 0b00100;
   }
    if(digitalRead(12)){
-    buttonState ^= 0b0010;
+    buttonState ^= 0b00010;
   }
    if(digitalRead(13)){
-    buttonState ^= 0b0001;
+    buttonState ^= 0b00001;
   }
   
   Serial.println(buttonState);
